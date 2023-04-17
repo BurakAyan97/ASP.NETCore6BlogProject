5ad561d7b38579cf5d9c84b2b868156c5471cef5
BurakAyan97 <burakayan97@outlook.com>
2023-04-17 22:16:15 +0300

    updated

--- Blog.Data/UnitOfWorks/IUnitOfWork.cs
+++ Blog.Data/UnitOfWorks/IUnitOfWork.cs
@@ -11,6 +11,7 @@ namespace Blog.Data.UnitOfWorks
 {
     public interface IUnitOfWork : IAsyncDisposable
     {
+        //Tüm repositoryleri tek tek almak yerine generic şekilde oluşturuyoruz.
         IRepository<T> GetRepository<T>() where T : class,IBaseEntity, new();
         Task<int> SaveAsync();
         int Save();
